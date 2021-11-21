a9738b5daa03ca1cb03de66660dabb0137caac71
lurch9 <90292267+lurch9@users.noreply.github.com>
2021-11-20 20:55:57 -0800

    Added generateMarkdown function definition

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,35 +2,66 @@ function licenseBadge(data) {
 
   
   const licenseType = data.license[0];
-  let licenseString = "";
+  let license = "";
   switch(licenseType)
   {
     case 'MIT':
-      licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+      license = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
       break;
     case 'GNU General Public License 2.0':
-      licenseString = '![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)';
+      license = '![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)';
       break;
     case 'Apache License 2.0':
-      licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+      license = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
       break;
     case 'GNU General Public License 3.0':
-      licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+      license = `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
       break;
     case 'Unlicensed':
-      licenseString = '';
+      license = '';
       break;
   }
-  return licenseString;
+  return license;
   
-  };
+};
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+    return `# ${data.title}
+## Table of Contents:
+  1. [Description](#description) 
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)  
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [License](#License)
+  7. [GitHub](#GitHub)
+  8. [E-mail](#E-mail)
 
-`;
+## Description
+${data.description} 
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## License
+${licenseBadge(data)}
+
+## GitHub
+${data.github}
+
+## E-mail
+${data.email}`
 }
-
 module.exports = generateMarkdown;
 
