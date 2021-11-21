370b1516fbdb0c5ec05fac48f28fe7fcc04a7390
lurch9 <90292267+lurch9@users.noreply.github.com>
2021-11-20 21:11:37 -0800

    Added inquirer questions to init function

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-// TODO: Include packages needed for this application
+function init () {
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    const inquirer = require('inquirer');
+    const fs = require('fs'); 
+    const generate = require('./utils/generateMarkdown.js');
+    const path = require('path');
+    
+    inquirer
+      .prompt([
+        { 
+          type: "input",
+          message: "Please Enter a Title for Your Project: ",
+          name: "title",  
+             },
+        { 
+          type: "input",
+          message: "Please Enter a Description for Your Project: ",
+          name: "description",  
+             },
+        {
+           type: "input",
+           message: "Please Enter Installation Instructions for Your Project: ",
+           name: "installation",
+        },
+        {
+            type: "input",
+            message: "Please Enter the Typical Use of Your Project: ",
+            name: "usage",
+        },
+        {
+            type: "checkbox",
+            message: "Which license, if any, did you use for this repository?",
+            choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0", "Unlicensed"],
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "How can people Contribute to your project?",
+            name: "contributing",
+        },
+       {
+            type: "input",
+            message: "How do people update the tests for your project?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "What is your email address where users and contributors can send questions?",
+            name: "email"
+        },
+      ])
+      
+    }
+    
+    init();
 
